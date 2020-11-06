7bbd6fbf29b592d893faae52ae5f487a7599abb1
Yogesh Shinde <FWIN01962@fulcrumww.com>
2020-11-06 18:42:37 +0530

    YS : Committed

--- JWTAuthentication_Service/Startup.cs
+++ JWTAuthentication_Service/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using JWTAuthentication_Service.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace JWTAuthentication_Service
 {
@@ -26,6 +20,15 @@ namespace JWTAuthentication_Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod());
+            });
+
+            services.AddCustomSwagger();
+            //services.AddScoped<IRuleEngineService, RuleEngineService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,11 +39,18 @@ namespace JWTAuthentication_Service
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+
+            app.UseSwagger();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                                  "JWT Authentication Service");
+            });
 
             app.UseEndpoints(endpoints =>
             {
